55b41c989fb8be1cb6262c432137beaed4b532f9
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-08-16 18:46:12 +0300

    exercise 11.9: cypress e2e

--- cypress/e2e/e2e_test.cy.js
+++ cypress/e2e/e2e_test.cy.js
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('can open pokemon page', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
